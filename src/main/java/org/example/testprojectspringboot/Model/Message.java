6b3e97036d9e70aa20f65c5f93a4b8ee5c0a65d1
Yinli <jiangyinli0803@gmail.com>
2025-04-15 23:06:42 +0200

    Get, add and delete message in Controller

---
+++ src/main/java/org/example/testprojectspringboot/Model/Message.java
@@ -0,0 +1,13 @@
+package org.example.testprojectspringboot.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Message {
+    String id;
+    String name;
+    String message;
+
+   }
